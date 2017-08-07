b097c4d29e5e528605aee9d5fcb1c35e06ed3318
Alexandre Brandão Lustosa <abrandao@stone.com.br>
2017-08-06 21:58:21 -0300

    Adicionado suporte multitarget

---
+++ RabbitMQSimpleConnector/Library/ChannelFactory.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQSimpleConnector.Entity;
+
+
+namespace RabbitMQSimpleConnector.Library {
+    public static class ChannelFactory {
+
+        private static readonly object SyncObj = new object();
+
+        private static IConnection _connection;
+
+        public static IModel Create(ConnectionConfig connectionConfig) {
+
+            var factory = new ConnectionFactory() {
+                HostName = connectionConfig.HostName,
+                VirtualHost = connectionConfig.VirtualHost,
+                UserName = connectionConfig.UserName,
+                Password = connectionConfig.Password,
+                AutomaticRecoveryEnabled = true
+            };
+
+            if (_connection == null) {
+                lock (SyncObj) {
+                    if (_connection == null) {
+                        _connection = factory.CreateConnection();
+                    }
+                }
+            }
+
+            var channel = _connection.CreateModel();
+
+            return channel;
+        }
+
+        private static void CloseConnection()
+        {
+            _connection.Close();
+            _connection.Dispose();
+        }
+    }
+}
