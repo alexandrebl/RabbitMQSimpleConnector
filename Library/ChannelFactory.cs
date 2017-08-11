5bd80383df52701a467cd6cb058328534e6fb2e7
Eduardo Tenório de Souza <esouza@stone.com.br>
2017-08-11 20:52:56 -0300

    Refatoração - Adiciona testes; -Melhorias no código fonte; -Dividido a responsabilidade por classes.

--- Library/ChannelFactory.cs
+++ Library/ChannelFactory.cs
@@ -1,17 +1,28 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQSimpleConnector.Entity;
 
-
 namespace RabbitMQSimpleConnector.Library {
+    /// <summary>
+    /// Responsável por criar conexões com RabbitMQ
+    /// </summary>
     public static class ChannelFactory {
-
+        /// <summary>
+        /// Objeto
+        /// </summary>
         private static readonly object SyncObj = new object();
 
+        /// <summary>
+        /// Interface de conexão com RabbitMQ
+        /// </summary>
         private static IConnection _connection;
 
-        public static IModel Create(ConnectionConfig connectionConfig) {
-
-            var factory = new ConnectionFactory() {
+        /// <summary>
+        /// Método cria uma conexão com RabbitMQ
+        /// </summary>
+        /// <param name="connectionConfig">Configurações de conexão</param>
+        /// <returns></returns>
+        public static IModel Create(ConnectionSetting connectionConfig) {
+            var factory = new ConnectionFactory {
                 HostName = connectionConfig.HostName,
                 VirtualHost = connectionConfig.VirtualHost,
                 UserName = connectionConfig.UserName,
@@ -32,10 +43,13 @@ namespace RabbitMQSimpleConnector.Library {
             return channel;
         }
 
-        private static void CloseConnection()
-        {
-            _connection.Close();
-            _connection.Dispose();
+        /// <summary>
+        /// Método responsável por fechar a conexão com RabbitMQ
+        /// </summary>
+        public static void CloseConnection() {
+            _connection?.Close();
+            _connection?.Dispose();
+            _connection = null;
         }
     }
 }
