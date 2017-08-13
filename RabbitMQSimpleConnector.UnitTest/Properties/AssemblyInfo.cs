4485d9b6f093c3639741af5dd5dd2ee593f1af46
Eduardo Tenório de Souza <esouza@stone.com.br>
2017-08-13 16:10:17 -0300

    Melhoria continua - Adicionado camada de testes; - Adicionado classe BaseQueue; - Adicionado classe Rpc(ainda não implementado); - Alterado da pasta entity para library as classes producer e consumer.

---
+++ RabbitMQSimpleConnector.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RabbitMQSimpleConnector.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RabbitMQSimpleConnector.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f9ad4cd-451e-4705-b128-d0e53d5440d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
