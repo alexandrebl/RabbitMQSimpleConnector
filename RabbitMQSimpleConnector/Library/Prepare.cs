04e197d39ab5fa8c2f4cb31312cc8d716dc5643d
Alexandre Brandão Lustosa <abrandao@stone.com.br>
2018-08-30 00:18:17 -0300

    Adicionada classe de criacao de filas

---
+++ RabbitMQSimpleConnector/Library/Prepare.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQSimpleConnector.Domain;
+using System.Collections.Generic;
+
+namespace RabbitMQSimpleConnector.Library {
+    public static class Prepare {
+
+        public static void QueueDeclareAndBind(IModel channel, PrepareConfiguration prepareConfiguration) {
+            var queueArguments = new Dictionary<string, object>();
+
+            if (!string.IsNullOrWhiteSpace(prepareConfiguration.DeadLetterRouteName)) {
+                queueArguments.Add("x-dead-letter-exchange", prepareConfiguration.ExchangeName);
+                queueArguments.Add("x-dead-letter-routing-key", prepareConfiguration.DeadLetterRouteName);
+            }
+
+            if (prepareConfiguration.Lazy) {
+                queueArguments.Add("x-queue-mode", "lazy");
+            }
+
+            if (prepareConfiguration.MaxPriority != null) {
+                queueArguments.Add("x-max-priority", prepareConfiguration.MaxPriority.Value);
+            }
+
+            QueueDeclare(channel, prepareConfiguration, queueArguments);
+            QueueBind(channel, prepareConfiguration);
+        }
+
+        public static void QueueDeclare(IModel channel, PrepareConfiguration prepareConfiguration,
+            IDictionary<string, object> queueArguments) {
+            channel.QueueDeclare(prepareConfiguration.QueueName, arguments: queueArguments);
+        }
+
+        public static void QueueBind(IModel channel, PrepareConfiguration prepareConfiguration) {
+            channel.QueueBind(prepareConfiguration.QueueName, prepareConfiguration.ExchangeName, prepareConfiguration.RouteName);
+        }
+    }
+}
