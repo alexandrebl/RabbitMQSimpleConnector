049776056ce417f315b3327baf429b28391f7ee0
Alexandre Brandão Lustosa <abrandao@stone.com.br>
2018-09-01 03:26:47 -0300

    Adicionado GenericPublisher

--- RabbitMQSimpleConnector/Library/GenericProducer.cs
+++ RabbitMQSimpleConnector/Library/GenericProducer.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQSimpleConnector.Entity;
 
 namespace RabbitMQSimpleConnector.Library {
-    public static class GenericProducer<T> {
+    public class GenericProducer
+    {
+        private readonly IModel _channel;
+
+        public GenericProducer(ConnectionSetting connectionSetting)
+        {
+            _channel = ChannelFactory.Create(connectionSetting);
+        }
 
         /// <summary>
         /// Publica a mensagem na fila
         /// </summary>
         /// <param name="obj"></param>
-        public static void GenericPublish(T obj, IModel channel = null, string exchange = null, string routingKey = null) {
+        public void Publish<T>(T obj, string exchange = null, string routingKey = null, IBasicProperties basicProperties = null) {
             var data = JsonConvert.SerializeObject(obj);
             var buffer = Encoding.UTF8.GetBytes(data);
 
-            channel.BasicPublish(exchange: exchange ?? "", routingKey: routingKey, basicProperties: null, body: buffer);
+            _channel.BasicPublish(exchange: exchange ?? "", routingKey: routingKey, basicProperties: basicProperties, body: buffer);
         }
     }
 }
